0716a1c03937de477dd2e8d97ed8eba922a835d8
shivom007 <shivmahto37@gmail.com>
2024-01-12 04:10:09 +0530

    added dockerfile

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('trust proxy', 1);
 //Database connection establishing
 mongoose
   .connect(process.env.MONGO_URI,{
